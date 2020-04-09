58a9681029915e175a0d9dde7b62ffa6cfbe2751
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-09 14:52:14 +0800

    Update hdfs_test.py

--- code/hdfs_test.py
+++ code/hdfs_test.py
@@ -5,8 +5,11 @@ mongodb_client = MongoClient('172.28.0.2:27017')
 db = mongodb_client["judicial_test"]
 collect = db["judi"]
 
-for record in collect.find():
-    pprint.pprint(record)
+print(db)
+print(collect)
+
+# for record in collect.find():
+#     print(record)
 
 # client = Client("https://172.28.0.3:50070")
 # client.status("/")
