ceb18fad012e6765eba09ab627b7b2c6715e4b5c
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-09 14:39:39 +0800

    Update hdfs_test.py

--- code/hdfs_test.py
+++ code/hdfs_test.py
@@ -1,4 +1,12 @@
+from pymongo import MongoClient
 from hdfs import *
 
-client = Client("https://172.28.0.3:50070")
-client.status("/")
+mongodb_client = MongoClient('172.28.0.2:27017')
+db = mongodb_client["judicial_test"]
+collect = db["judi"]
+
+for record in collect.find():
+    print(record)
+
+# client = Client("https://172.28.0.3:50070")
+# client.status("/")
