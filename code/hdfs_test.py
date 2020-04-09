bd76f1e07ce4f7fd1ad42ffa30cc89b28d9c8db7
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-09 15:09:01 +0800

    Update hdfs_test.py

--- code/hdfs_test.py
+++ code/hdfs_test.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from hdfs import *
 
 mongodb_client = MongoClient('172.28.0.2:27017')
-db = mongodb_client["judicial_test"]
+db = mongodb_client["judicial"]
 collect = db["judi"]
 
 for record in collect.find():
