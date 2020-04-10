30bf374c50c330d3926628185ca6739524249478
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-10 23:45:09 +0800

    Update hdfs_test.py

--- code/hdfs_test.py
+++ code/hdfs_test.py
@@ -2,12 +2,12 @@ from pymongo import MongoClient
 from hdfs import *
 
 mongodb_client = MongoClient('172.28.0.2:27017')
-db = mongodb_client["judicial"]
-collect = db["judi"]
+db = mongodb_client.judicial
+collect = db.judi
 
 data = collect.find()
-print(data)
-
+print(data.count_documents({}))
+print(len(list(data)))
 
 # client = Client("http://172.28.0.3:50070")
 # client.status("/")
