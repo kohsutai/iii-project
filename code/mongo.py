ca998ff7f728856cdcb7f098145cc292e3c2a070
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-11 21:27:02 +0800

    Update mongo.py

--- code/mongo.py
+++ code/mongo.py
@@ -1,13 +1,20 @@
 import json
 from pymongo import MongoClient
+import re
 from hdfs import *
 
+
 mongodb_client = MongoClient('172.28.0.2:27017')
-db = mongodb_client.judicial
-collection = db.judi
+db = mongodb_client['iii-project']
+collection = db['judicial']
 
-mongo_output = json.dumps(collection)
-print(mongo_output)
+output = []
+for i in collection.find():
+    i['_id']=re.findall("'(.*)'",i.get('_id').__repr__())[0]
+    output.append(i)
 
+with open('/home/data/mongo_data.json', 'w', encoding="UTF-8") as f:
+    f.write(json.dumps(output, indent=2))
+   
 # client = Client("http://172.28.0.3:50070")
 # client.status("/")
