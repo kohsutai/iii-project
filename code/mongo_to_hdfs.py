0f91e211f27d2a2487bd9e40cebda625a8eca194
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-12 21:59:53 +0800

    Update and rename mongo.py to mongo_to_hdfs.py

--- code/mongo_to_hdfs.py
+++ code/mongo_to_hdfs.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 import json
 from bson.json_util import dumps
+from hdfs import Client
 
 mongodb_client = MongoClient('172.28.0.2:27017')
 db = mongodb_client['iii-project']
 collection = db['judicial']
-
 hdfs_client = Client("http://172.28.0.3:50070")
 
 # Export raw data from mongodb
@@ -15,3 +15,7 @@ for i in collection.find({},{'_id':False}):
 
 with open("/home/data/raw_data.json",'w',encoding='utf-8') as f:
   f.write(dumps(output, ensure_ascii=False))
+
+# Send raw data from mongodb to hdfs
+hdfs_client.makedirs("/user/cloudera/data", permission=777)
+# hdfs_client.upload("/user/cloudera/data", "/home/cloudera/iii-project/data")
