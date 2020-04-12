6540b7944272e41b2a3d301e2abc9035510d98e4
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-12 22:56:32 +0800

    Update mongo_to_hdfs.py

--- code/mongo_to_hdfs.py
+++ code/mongo_to_hdfs.py
@@ -18,4 +18,4 @@ with open("/home/data/raw_data.json",'w',encoding='utf-8') as f:
 
 # Send raw data from mongodb to hdfs
 hdfs_client.makedirs("/user/cloudera/iii-project/data", permission=777)
-hdfs_client.upload("/user/cloudera/iii-project/data","/home/cloudera/iii-project/data")
+hdfs_client.upload("/user/cloudera/iii-project/data","/data")
