e7ba09832e28b0ac9a5d5d63f98d8c8322a7dff8
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-12 20:49:39 +0800

    Update mongo_to_hdfs.py

--- code/mongo_to_hdfs.py
+++ code/mongo_to_hdfs.py
@@ -6,19 +6,17 @@ from bson.json_util import dumps
 mongodb_client = MongoClient('172.28.0.2:27017')
 db = mongodb_client['iii-project']
 collection = db['judicial']
-mongo_path = "/home/data/raw_data.json"
 
 hdfs_client = Client("http://172.28.0.3:50070")
-hdfs_path = "/user/cloudera/data"
 
 # Export raw data from mongodb
 output = []
 for i in collection.find({},{'_id':False}):
     output.append(i)
 
-with open(mongo_path,'w',encoding='utf-8') as f:
+with open("/home/data/raw_data.json",'w',encoding='utf-8') as f:
   f.write(dumps(output, ensure_ascii=False))
 
 # Send raw data from mongodb to cloudera hdfs
-hdfs_client.makedirs(hdfs_path, permission=777)
-hdfs_client.upload(hdfs_path, mongo_path)
+hdfs_client.makedirs("/user/cloudera/data", permission=777)
+hdfs_client.upload("/user/cloudera/data", "/home/cloudera/iii-project/data")
