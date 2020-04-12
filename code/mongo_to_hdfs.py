1d0e98cf622114aab613052a3ada01afda61c466
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-12 22:19:23 +0800

    Update mongo_to_hdfs.py

--- code/mongo_to_hdfs.py
+++ code/mongo_to_hdfs.py
@@ -6,7 +6,7 @@ from hdfs import Client
 mongodb_client = MongoClient('172.28.0.2:27017')
 db = mongodb_client['iii-project']
 collection = db['judicial']
-hdfs_client = Client("http://172.28.0.3:50070")
+hdfs_client = Client("http://172.28.0.3:50070",root="/")
 
 # Export raw data from mongodb
 output = []
