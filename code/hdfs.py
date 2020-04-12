211a8ad24e6a6683e35accefe7c24f40757f8b22
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-12 22:00:52 +0800

    Delete hdfs.py

--- code/hdfs.py
+++
@@ -1,5 +0,0 @@
-from hdfs import Client
-
-hdfs_client = Client("http://172.28.0.3:50070")
-hdfs_client.makedirs("/user/cloudera/data", permission=777)
-hdfs_client.upload("/user/cloudera/data", "/home/cloudera/iii-project/data")
