8b43df3237644898543c0a2105aa6cb667d0e50c
kohsutai <31853202+kohsutai@users.noreply.github.com>
2020-04-16 16:45:57 +0800

    Update spark_test.py

--- code/spark_test.py
+++ code/spark_test.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     sqlContext = SQLContext(sc)
     df = sqlContext.read.json("hdfs://localhost/user/cloudera/data/clean_data.json")
 
-    df.show()   # 顯示DataFrame
-    df.select(df['judge_content']).show()   # 顯示"judge_content"的內容
+    df.show()
+    df.select(df['judge_content']).show()
